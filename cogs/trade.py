edffdb3c7eb7e1ca68e5566bc6e4f8bb8cc5f82d
im-deek <52243166+im-deek@users.noreply.github.com>
2021-03-04 22:22:27 +0000

    Create trade.py

---
+++ cogs/trade.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+from pymongo import MongoClient
+from utils import myfunctions
+import os
+
+cluster = MongoClient(os.environ.get("TODD_MONGO_PREFIX"))
+database = cluster["todd-data"]
+collection = database["guildinfo"]
+
+class Trade(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def trade(self, ctx, command = None, value1 = None, value2 = None, value3 = None):
+        # planned commands: channel, add, close, remove
+        if command == "auth" and ctx.author.id == 321732493764067329:
+
+            guild = ctx.guild
+            results = collection.find({"guild": guild.id})
+
+            if value1 == None or value1.lower() == "false":
+                auth = False
+            elif value1.lower() == "true":
+                auth = True
+
+            if not list(results):
+                all = collection.find({})
+                total = len(list(all))
+                collection.insert_one({"_id": total, "guild": guild.id, "name": guild.name, "trading": auth})
+            else:
+                collection.update_one({"guild": guild.id}, {"$set": {"trading": auth}})
+
+            response = discord.Embed(title="Trading status updated", description=f"Changed to `{auth}`")
+            await ctx.send(embed=response)
+
+
+
+
+
+def setup(client):
+    client.add_cog(Trade(client))
